84ca9ea2615a8602c87685f782250c5c2f6d57e8
Matthias Meßmer <matthias.messmer@angelaschule-osnabrueck.net>
2020-11-14 01:47:31 +0100

    some more logic and comments

--- main.go
+++ main.go
@@ -9,10 +9,41 @@ import (
 	"github.com/gosimple/slug"
 )
 
+// 6 arrays holding the data
+// first line for the csv header
+var classes = [][]string{
+	{
+		"class_id",
+		"class_number",
+		"course_id",
+		"instructor_id",
+		"instructor_id_2",
+		"instructor_id_3",
+		"location_id",
+	},
+}
+
+var courses = [][]string{
+	{
+		"course_id",
+		"course_number",
+		"course_name",
+		"location_id",
+	},
+}
+
 var locations = [][]string{
 	{
 		"location_id",
 		"location_name",
+	},
+}
+
+var rosters = [][]string{
+	{
+		"roster_id",
+		"class_id",
+		"student_id",
 	},
 }
 
@@ -44,6 +75,7 @@ var students = [][]string{
 	},
 }
 
+// write data into csv file
 func writeCsv(data [][]string, filename string) {
 	outFile, err := os.Create(filename)
 	if err != nil {
@@ -58,6 +90,10 @@ func writeCsv(data [][]string, filename string) {
 	}
 }
 
+// write a template file
+// see: template/t flag with ">" pipe
+// enter your data into the template file
+// TODO: filename
 func generateTemplate() {
 	records := [][]string{
 		{
@@ -81,7 +117,19 @@ func generateTemplate() {
 	}
 }
 
+// check for duplicates
+func findInSlice(slice [][]string, id string) bool {
+	for _, row := range slice[1:] {
+		if row[0] == id {
+			return true
+		}
+	}
+	return false
+}
+
 func generateSixpack() {
+	// read the template file
+	// TODO: get filename from commandline or dropping file
 	inFile, err := os.Open("template.csv")
 	if err != nil {
 		log.Fatalln(err)
@@ -94,22 +142,49 @@ func generateSixpack() {
 		log.Fatalln(err)
 	}
 
+	// parse the data
 	// drop header line
 	for _, row := range records[1:] {
-		locationID := slug.Make(row[8])
+
+		// columns which are needed several times
 		studentID := row[0]
-		locations = append(locations, []string{locationID, row[8]})
-		staff = append(staff, []string{row[6], "", "Lehrer", row[6], "", "", locationID})
-		students = append(students, []string{studentID, "", row[1], "", row[2], "", "", "", "", locationID})
+		locationID := slug.Make(row[8])
+
+		if !findInSlice(classes, row[3]) {
+			classes = append(classes, []string{row[3], "", row[4], "", "", "", locationID})
+		}
+
+		if !findInSlice(courses, row[4]) {
+			courses = append(courses, []string{row[4], "", "", locationID})
+		}
+
+		if !findInSlice(locations, locationID) {
+			locations = append(locations, []string{locationID, row[8]})
+		}
+
+		if !findInSlice(rosters, row[5]) {
+			rosters = append(rosters, []string{row[5], row[3], studentID})
+		}
+
+		if !findInSlice(staff, row[6]) {
+			staff = append(staff, []string{row[6], "", "Lehrer", row[6], "", "", locationID})
+		}
+
+		students = append(students, []string{studentID, "", row[1], "", row[2], "", "", "", row[7], locationID})
 	}
-	// fmt.Println(locations)
+
+	// write the 6 files for upload
+	writeCsv(classes, "classes.csv")
+	writeCsv(courses, "courses.csv")
 	writeCsv(locations, "locations.csv")
+	writeCsv(rosters, "rosters.csv")
 	writeCsv(staff, "staff.csv")
 	writeCsv(students, "students.csv")
 }
 
 func main() {
 
+	// cmd flags
 	var template bool
 
 	flag.BoolVar(&template, "template", false, "Generate csv template")
@@ -117,6 +192,7 @@ func main() {
 
 	flag.Parse()
 
+	// flag switch
 	if template {
 		generateTemplate()
 	} else {
